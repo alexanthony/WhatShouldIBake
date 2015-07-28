823756a37b9a78fa55bb7d26b8d5efc8ec880ca3
Alex Anthony <alex.anthony28991@gmail.com>
2015-07-28 21:55:22 +1200

    Don't set a minimum height, but do some fancy js to stop the img collapsing before the loading gif loads (which looks like a flicker). Minimum height makes images squash/stretch on mobiles.

--- client/app/loadingSrc/loadingSrc.directive.js
+++ client/app/loadingSrc/loadingSrc.directive.js
@@ -14,15 +14,25 @@ angular.module('whatShouldIbakeApp')
           // Load up new image
           var img = new Image();
           img.src = value;
-          // Clear the old image instantly
+          // Clear the old image instantly, but save its height
+          var height = element[0].clientHeight;
           element[0].src = '';
+          if (height > 0) {
+            // Set the element to have a fixed height while there's no image loaded
+            element[0].style.height = height+'px';
+          }
           // Set loading image while we wait
           element[0].src = loadingImgSrc;
           // Once image is loaded, replace displayed image with this one
+          element[0].onload = function() {
+            element[0].style.height = 'auto';
+            element[0].onload = null;
+          };
           img.onload = function () {
               img.onload = null;
               if (element[0].src !== img.src) {
-                  element[0].src = img.src;
+                element[0].src = img.src;
+                element[0].style.height = 'auto';
               }
           };
         });
